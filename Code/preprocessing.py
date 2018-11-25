c7f79e7c8c8526cc91bf9c521aaddd3bfc8dfc5a
Stefan <swdriessen@gmail.com>
2018-11-25 10:35:29 +0100

    I get by with a little help from my friends, thanks Vincent Munos。

---
+++ Code/preprocessing.py
@@ -0,0 +1,143 @@
+import re
+import numpy as np
+import pandas as pd
+import graphviz
+import IPython
+import math
+
+# from sklearn_pandas import DataFrameMapper
+from sklearn.preprocessing import LabelEncoder, Imputer, StandardScaler
+from pandas.api.types import is_string_dtype, is_numeric_dtype
+from sklearn.ensemble import forest
+from sklearn.tree import export_graphviz
+from scipy.cluster import hierarchy as hc
+import matplotlib.pyplot as plt
+
+def display_all(df):
+    with pd.option_context('display.max_rows', 1000):
+        with pd.option_context('display.max_columns', 1000):
+            display(df)
+
+def add_datepart(df, fldname):
+    # remember to use parse_dates=<date_column> in pd.read_csv
+    fld = df[fldname]
+    targ_pre = re.sub('[Dd]ate$', '', fldname)
+    for n in [
+        'Year', 'Quarter', 'Month', 'Week', 'Day', 'Dayofweek', 'Dayofyear', 'Is_month_end', 'Is_month_start',
+        'Is_quarter_end', 'Is_quarter_start', 'Is_year_end', 'Is_year_start']:
+        df[targ_pre + n] = getattr(fld.dt, n.lower())
+    df[targ_pre + 'Elapsed'] = (fld - fld.min()).dt.days
+    df.drop(fldname, axis=1, inplace=True)
+    
+def train_cats(df):
+    for n, c in df.items():
+        if is_string_dtype(c):
+            df[n] = c.astype('category').cat.as_ordered()
+            
+def proc_df(df, y_fld=None, skip_flds=None, ignore_flds=None, do_scale=False, na_dict=None, preproc_fn=None, max_n_cat=None, subset=None, mapper=None):
+    if not ignore_flds: ignore_flds=[]
+    if not skip_flds: skip_flds=[]
+    if subset: df = get_sample(df,subset)
+    else: df = df.copy()
+    ignored_flds = df.loc[:, ignore_flds]
+    df.drop(ignore_flds, axis=1, inplace=True)
+    if preproc_fn: preproc_fn(df)
+    if y_fld is None: y = None
+    else:
+        if not is_numeric_dtype(df[y_fld]): df[y_fld] = df[y_fld].cat.codes
+        y = df[y_fld].values
+        skip_flds += [y_fld]
+    df.drop(skip_flds, axis=1, inplace=True)
+
+    if na_dict is None: na_dict = {}
+    else: na_dict = na_dict.copy()
+    na_dict_initial = na_dict.copy()
+    for n,c in df.items(): na_dict = fix_missing(df, c, n, na_dict)
+    if len(na_dict_initial.keys()) > 0:
+        df.drop([a + '_na' for a in list(set(na_dict.keys()) - set(na_dict_initial.keys()))], axis=1, inplace=True)
+    if do_scale: mapper = scale_vars(df, mapper)
+    for n,c in df.items(): numericalize(df, c, n, max_n_cat)
+    df = pd.get_dummies(df, dummy_na=True)
+    df = pd.concat([ignored_flds, df], axis=1)
+    res = [df, y, na_dict]
+    if do_scale: res = res + [mapper]
+    return res
+
+def apply_cats(df, trn):
+    for n, c in df.items():
+        if trn[n].dtype.name == 'category':
+            df[n] = pd.Categorical(c, categories=trn[n].cat.categories, ordered=True)
+            
+# need to order categories
+# df.column.cat.set_categories(['High', 'Medium', 'Low'], ordered=True, inplace=True)
+def fix_missing(df, col, name, na_dict):
+    if is_numeric_dtype(col):
+        if pd.isnull(col).sum() or (name in na_dict):
+            df[name+'_na'] = pd.isnull(col)
+            filler = na_dict[name] if name in na_dict else col.median()
+            df[name] = col.fillna(filler)
+            na_dict[name] = filler
+    return na_dict
+
+def numericalize(df, col, name, max_n_cat):
+    if not is_numeric_dtype(col) and ( max_n_cat is None or len(col.cat.categories)>max_n_cat):
+        df[name] = col.cat.codes+1
+        
+def get_sample(df,n):
+    idxs = sorted(np.random.permutation(len(df))[:n])
+    return df.iloc[idxs].copy()
+
+def draw_tree(t, df, size=10, ratio=0.6, precision=0):
+    """ Draws a representation of a random forest in IPython.
+    Parameters:
+    -----------
+    t: The tree you wish to draw
+    df: The data used to train the tree. This is used to get the names of the features.
+    """
+    s=export_graphviz(t, out_file=None, feature_names=df.columns, filled=True,
+                      special_characters=True, rotate=True, precision=precision)
+    IPython.display.display(graphviz.Source(re.sub('Tree {',
+       f'Tree {{ size={size}; ratio={ratio}', s)))
+    
+def set_rf_samples(n):
+    """ Changes Scikit learn's random forests to give each tree a random sample of
+    n random rows.
+    """
+    forest._generate_sample_indices = (lambda rs, n_samples:
+        forest.check_random_state(rs).randint(0, n_samples, n))
+    
+def reset_rf_samples():
+    """ Undoes the changes produced by set_rf_samples.
+    """
+    forest._generate_sample_indices = (lambda rs, n_samples:
+        forest.check_random_state(rs).randint(0, n_samples, n_samples))
+    
+def split_vals(a,n):
+    return a[:n].copy(), a[n:].copy()
+
+def split_vals_test(a, n1, n2):
+    return a[:n1].copy(), a[n1:n2].copy(), a[n2:].copy()
+
+def rmse(x, y): return math.sqrt(((x-y)**2).mean())
+
+def print_score(m, X_train, y_train, X_valid, y_valid):
+    res = [rmse(m.predict(X_train), y_train), rmse(m.predict(X_valid), y_valid),
+          m.score(X_train, y_train), m.score(X_valid, y_valid)]
+    if hasattr(m, 'oob_score_'): res.append(m.oob_score_)
+    print(res)
+    
+def rf_feat_importance(m, df):
+    return pd.DataFrame({'cols':df.columns, 'imp':m.feature_importances_}
+                       ).sort_values('imp', ascending=False)
+
+def plot_fi(fi): 
+    return fi.plot('cols','imp','barh', figsize=(12,7), legend=False)
+
+def dendrogram(df):
+    corr = np.round(scipy.stats.spearmanr(df).correlation, 4)
+    corr_condensed = hc.distance.squareform(1-corr)
+    z = hc.linkage(corr_condensed, method='average')
+    fig = plt.figure(figsize=(16,10))
+    dendrogram = hc.dendrogram(z, labels=df.columns, 
+          orientation='left', leaf_font_size=16)
+    plt.show()
